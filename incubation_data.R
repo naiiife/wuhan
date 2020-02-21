290592593bcba83e840a59a7c0ca311b143ef24e
naiiife <61304007+naiiife@users.noreply.github.com>
2020-02-21 16:21:01 +0800

    R code for real data

--- incubation_data.R
+++ incubation_data.R
@@ -11,42 +11,66 @@ x = x+0.1
 
 
 ##########################################gamma incubation
-f <- function(y,a,b){
- return(dgamma(y,shape=a,rate=b))
-}
-h <- function(y,a,b){
- RE = b/a*(1-pgamma(y,shape=a,rate=b))
- return(RE)
-}
-##########################################weibull incubatiom
 #f <- function(y,a,b){
-#  return(dweibull(y,shape=a,scale=b))
+# return(dgamma(y,shape=a,rate=b))
 #}
 #h <- function(y,a,b){
-#  RE = (1-pweibull(y,shape=a,scale=b))/gamma(1+1/a)/b
-#  return(RE)
+# RE = b/a*(1-pgamma(y,shape=a,rate=b))
+# return(RE)
 #}
+#F <- function(k,a,b,p){
+# if (k<=0) return(0)
+# else{
+# REf = pgamma(k,shape=a,rate=b)
+# REh = pgamma(k,shape=a+1,rate=b)+k*b/a*(1-pgamma(k,shape=a,rate=b))
+# return(REf*(1-p)+REh*p)
+# }
+#}
+##########################################weibull incubation
+f <- function(y,a,b){
+  return(dweibull(y,shape=a,scale=b))
+}
+h <- function(y,a,b){
+  RE = (1-pweibull(y,shape=a,scale=b))/gamma(1+1/a)/b
+  return(RE)
+}
+F <- function(k,a,b,p){
+ if (k<=0) return(0)
+ else{
+ REf = pweibull(k,shape=a,scale=b)
+ REh = pgamma((k/b)^a,shape=1/a,rate=1)
+ return(REf*(1-p)+REh*p)
+}
+}
 ##########################################lognormal incubation
-f <- function(y,u,s){
-  return(dnorm(log(y),u,s)/y)
-}
-h <- function(y,u,s){
-  RE = exp(-u-s^2/2)*(1-pnorm(log(y),u,s))
-  RE =return(RE)
-}
+#f <- function(y,u,s){
+#  return(dnorm(log(y),u,s)/y)
+#}
+#h <- function(y,u,s){
+#  RE = exp(-u-s^2/2)*(1-pnorm(log(y),u,s))
+#  RE =return(RE)
+#}
+#F <- function(k,u,s,p){
+# if (k<=0) return(0)
+# else{
+# REf = pnorm(log(k),u,s)
+# REh = pnorm(log(k),u+s^2,s)+exp(-u-s^2/2)*k*(1-pnorm(log(k),u,s))
+# return(REf*(1-p)+REh*p)
+#}
+#}
 
 ## Inital for gamma
 #a = 6
 #b = 0.8
 #p = 0.8
 ## Init for weibull
-#a = 2
-#b = 7
-#p = 0.8
-## Inital for lognormal
-a = 1.8
-b = 0.4
+a = 2
+b = 7
 p = 0.8
+## Inital for lognormal
+#a = 1.8
+#b = 0.4
+#p = 0.8
 
 
 ## Iter
@@ -58,8 +82,8 @@ for (k in 1:1000){
  loglik <- function(pa){
    a = pa[1]
    b = pa[2]
-   RE = - sum( (1-g[1:m])*log(f(x[1:m],a,b)) + g[1:m]*log(h(x[1:m],a,b)) )
-   #RE = - sum( (1-g)*log(f(x,a,b)) + g*log(h(x,a,b)) )
+   RE = - sum( (1-g[1:m])*log(f(x[1:m],a,b)) + g[1:m]*log(h(x[1:m],a,b)) ) #Unlabeled only
+   #RE = - sum( (1-g)*log(f(x,a,b)) + g*log(h(x,a,b)) )  #All data
    return(RE)
  }
  a0 = a
@@ -71,6 +95,18 @@ for (k in 1:1000){
  if (sum((a-a0)^2+(b-b0)^2)<1e-6) break
 }
 
+
+###goodness-of-fit
+Ei = rep(NA,17)
+Oi = rep(NA,17)
+for (k in 1:16){
+ Ei[k] = (F(k-0.4,a,b,p)-F(k-1.4,a,b,p))*m
+ Oi[k] = (sum(x[1:m]<k)-sum(x[1:m]<k-1))
+}
+Ei[17] = (1-F(16-0.4,a,b,p))*m
+Oi[17] = sum(x[1:m]>=16)
+sum((Oi-Ei)^2/Ei)
+qchisq(0.95,17-3-1)
 
 #########################gamma
 #round(c(a,b,1-p),2)
@@ -90,10 +126,9 @@ for (k in 1:1000){
 
 
 #seq0=seq(0.1,22,0.1)
-#hist(x[1:m],breaks=20,prob=T,ylim=c(0,0.2),main='log-normal incubation',
-#     xlab = "Time from departure to symptom onset")
+#hist(x[1:m],breaks=20,prob=T,ylim=c(0,0.2),
+#     xlab = "Time from departure to symptoms onset")
 #lines(seq0,p*h(seq0,a,b)+(1-p)*f(seq0,a,b),lwd=1.5)
 #lines(seq0,f(seq0,a,b),col='blue',lwd=2)
 #lines(seq0,h(seq0,a,b),col='red',lwd=1.5)
-
 
